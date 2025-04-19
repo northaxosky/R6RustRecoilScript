5ec8aa25f3f5e52f6cd6f57cb16af9f52e061987
Kuzey Gok <kuzeygokk@outlook.com>
2025-04-18 18:52:18 -0700

    Formatting changes

--- src/gui.rs
+++ src/gui.rs
@@ -1,7 +1,7 @@
 // Move GUI-related code to this module
-use std::sync::atomic::Ordering;
-use eframe::egui;
 use crate::recoil::{IS_ACTIVE, RECOIL_STRENGTH};
+use eframe::egui;
+use std::sync::atomic::Ordering;
 
 #[derive(Default)]
 pub struct MyApp {
@@ -20,8 +20,9 @@ impl eframe::App for MyApp {
         style.text_styles = [
             (egui::TextStyle::Heading, egui::FontId::proportional(40.0)), // Larger heading
             (egui::TextStyle::Body, egui::FontId::proportional(30.0)),    // Larger body text
-            (egui::TextStyle::Button, egui::FontId::proportional(30.0)),  // Define Button text style
-        ].into();
+            (egui::TextStyle::Button, egui::FontId::proportional(30.0)), // Define Button text style
+        ]
+        .into();
         ctx.set_style(style);
 
         egui::CentralPanel::default().show(ctx, |ui| {
@@ -35,18 +36,27 @@ impl eframe::App for MyApp {
                     // Add a spacer to ensure alignment
                     ui.add_space(1.0);
 
-                    if ui.add_sized([button_width, button_height], egui::Button::new("On")).clicked() {
+                    if ui
+                        .add_sized([button_width, button_height], egui::Button::new("On"))
+                        .clicked()
+                    {
                         IS_ACTIVE.store(true, Ordering::SeqCst);
                         println!("Recoil control activated");
                     }
 
-                    if ui.add_sized([button_width, button_height], egui::Button::new("Off")).clicked() {
+                    if ui
+                        .add_sized([button_width, button_height], egui::Button::new("Off"))
+                        .clicked()
+                    {
                         IS_ACTIVE.store(false, Ordering::SeqCst);
                         println!("Recoil control deactivated");
                     }
                 });
 
-                ui.label(format!("Status: {}", if self.is_active { "On" } else { "Off" }));
+                ui.label(format!(
+                    "Status: {}",
+                    if self.is_active { "On" } else { "Off" }
+                ));
 
                 ui.separator(); // Add a separator for better UI organization
 
@@ -58,7 +68,14 @@ impl eframe::App for MyApp {
                     .selected_text(selected_operator.clone())
                     .show_ui(ui, |ui| {
                         for operator in &operators {
-                            if ui.selectable_value(selected_operator, operator.to_string(), *operator).clicked() {
+                            if ui
+                                .selectable_value(
+                                    selected_operator,
+                                    operator.to_string(),
+                                    *operator,
+                                )
+                                .clicked()
+                            {
                                 println!("Selected operator: {}", selected_operator);
                             }
                         }
@@ -67,10 +84,16 @@ impl eframe::App for MyApp {
                 ui.separator(); // Add a separator for better UI organization
 
                 // Modify the slider to remove the "Strength" text and make it span the full width of the window
-                ui.label("Adjust Recoil Strength:".to_owned() + &RECOIL_STRENGTH.load(Ordering::SeqCst).to_string());
+                ui.label(
+                    "Adjust Recoil Strength:".to_owned()
+                        + &RECOIL_STRENGTH.load(Ordering::SeqCst).to_string(),
+                );
                 let mut recoil_strength = RECOIL_STRENGTH.load(Ordering::SeqCst);
                 ui.spacing_mut().slider_width = ui.available_width();
-                if ui.add(egui::Slider::new(&mut recoil_strength, 0..=20).show_value(false)).changed() {
+                if ui
+                    .add(egui::Slider::new(&mut recoil_strength, 0..=20).show_value(false))
+                    .changed()
+                {
                     RECOIL_STRENGTH.store(recoil_strength, Ordering::SeqCst);
                     println!("Recoil strength updated to: {}", recoil_strength);
                 }
